2ea9c6c14a229f06e01c38fd7a51894a8beb53ea
raymonddevries <raymond.l.devries@gmail.com>
2020-11-24 23:20:56 -0800

    Add basic url tests.

--- registration/urls.py
+++ registration/urls.py
@@ -5,7 +5,7 @@ from registration import views
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path(
-        "bmc_registration", views.BMCRegistrationView.as_view(), name="BMCRegistration"
+        "bmc_registration/", views.BMCRegistrationView.as_view(), name="BMCRegistration"
     ),
-    path("signup", views.UserRegistrationView.as_view(), name="signup"),
+    path("signup/", views.UserRegistrationView.as_view(), name="signup"),
 ]
---
+++ tests/test_with_client.py
@@ -0,0 +1,27 @@
+import pytest
+from django.contrib.auth.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+URLS = [
+    ("/", (200, 200)),
+    ("/bmc_registration/", (302, 200)),
+    ("/signup/", (200, 200)),
+    ("/accounts/login/", (200, 200)),
+    ("/accounts/password_change/", (302, 200)),
+    ("/accounts/password_change/done/", (302, 200)),
+]
+
+
+@pytest.mark.parametrize(["url", "status_code"], URLS)
+def test_anonymous_access(client, url, status_code):
+    response = client.get(url)
+    assert response.status_code == status_code[0]
+
+
+@pytest.mark.parametrize(["url", "status_code"], URLS)
+def test_logged_in_access(client, url, status_code):
+    client.force_login(User.objects.get_or_create(username="test_user")[0])
+    response = client.get(url)
+    assert response.status_code == status_code[1]
