7dc5e262a7be6be5916532076355083085937f65
raymonddevries <raymond.l.devries@gmail.com>
2020-11-25 23:25:27 -0800

    Add available classes to registration home.

--- registration/views.py
+++ registration/views.py
@@ -21,11 +21,22 @@ class Home(TemplateView):
 class RegistrationHome(LoginRequiredMixin, TemplateView):
     template_name = "bmc_registration/registration_home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["registration_complete"] = models.RegistrationForm.objects.filter(
+            user=self.request.user
+        ).exists()
+        context["available_classes"] = models.ClassType.objects.filter(
+            visible=True
+        ).order_by("requirement")
+
+        return context
+
 
 class RegistrationInfoForm(LoginRequiredMixin, FormView):
     template_name = "bmc_registration/registration_form.html"
     form_class = BMCRegistrationForm
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("registration_home")
 
     def get_form(self, form_class=None):
         try:
