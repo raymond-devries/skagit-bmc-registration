3e6168974cac132ba1cdf0eaf460204664eab488
raymonddevries <raymond.l.devries@gmail.com>
2021-01-09 13:10:33 -0800

    Update static files storage.

--- SkagitRegistration/settings.py
+++ SkagitRegistration/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGOUT_REDIRECT_URL = reverse_lazy("home")
 LOGIN_REDIRECT_URL = reverse_lazy("home")
