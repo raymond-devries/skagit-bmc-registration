90b25b20b758c9788e66202cc6d36b595f03a4ec
raymonddevries <raymond.l.devries@gmail.com>
2021-01-12 00:01:31 -0800

    Add instructor group on startup.

--- registration/apps.py
+++ registration/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class RegistrationConfig(AppConfig):
     name = "registration"
+
+    def ready(self):
+        from django.contrib.auth.models import Group
+
+        Group.objects.get_or_create(name="instructor")
