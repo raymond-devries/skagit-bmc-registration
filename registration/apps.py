0dbddec07b3f68dce4df39bdc98c1c6468704342
raymonddevries <raymond.l.devries@gmail.com>
2021-01-13 00:25:23 -0800

    Add group via migration instead of via app startup.

--- registration/apps.py
+++ registration/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 
 class RegistrationConfig(AppConfig):
     name = "registration"
-
-    def ready(self):
-        from django.contrib.auth.models import Group
-
-        from registration.models import INSTRUCTOR_GROUP
-
-        Group.objects.get_or_create(INSTRUCTOR_GROUP)
