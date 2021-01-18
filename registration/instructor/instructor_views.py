23fd7ca3406d0c9050118b9ebca25e7b0f0983ab
raymonddevries <raymond.l.devries@gmail.com>
2021-01-17 16:41:19 -0800

    Add individual registrant page.

--- registration/instructor/instructor_views.py
+++ registration/instructor/instructor_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from registration import models
@@ -21,4 +22,22 @@ class CurrentRegistrationsView(UserPassesTestMixin, TemplateView):
             .order_by("first_name", "last_name")
         )
         context["course_types"] = models.CourseType.objects.filter(visible=True)
+        return context
+
+
+class RegistrantView(UserPassesTestMixin, TemplateView):
+    template_name = "bmc_registration/instructor/registrant_view.html"
+
+    def test_func(self):
+        return self.request.user.profile.is_instructor
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        registrant = get_object_or_404(User, username=self.kwargs["username"])
+        context["registrant"] = registrant
+        context["courses"] = models.Course.objects.filter(participants=registrant)
+        context["wait_list_courses"] = models.Course.objects.filter(
+            waitlist__user=registrant
+        )
+        context["registration_form"] = models.RegistrationForm(user=registrant)
         return context
